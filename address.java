3e4eefdc3eec7363b5c97833864ae3ec169ccde4
ringol5046 <ringol5046@gmail.com>
2015-07-19 01:49:02 -0400

    edit question text

--- address.java
+++ address.java
@@ -1,8 +1,14 @@
-/*Implement a class Address . An address has a house number, a street, an optional apartment number,
- a city, a state, and a postal code. Supply two constructors: one with an apartment number and
- one without. Supply a print method that prints the address with the street on one line and the city,
- state, and zip code on the next line. Supply a method public boolean comesBefore(Address other)
- that tests whether this address comes before another when the addresses are compared by postal code.*/
+/*Implement a class Address . An address has 
+ -a house number
+ -a street
+ -an optional apartment number
+ -a city
+ -a state
+ -a postal code. 
+ Supply two constructors: one with an apartment number and one without. Supply a print method that 
+ prints the address with the street on one line and the city,state, and zip code on the next line. 
+ Supply a method public boolean comesBefore(Address other) that tests whether this address comes 
+ before another when the addresses are compared by postal code.*/
 
 public class address {
 	private int houseNumber;
