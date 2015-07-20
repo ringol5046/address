01a0cb7fce2ee095fc96930ab4e07fe2c99b0560
ringol5046 <ringol5046@gmail.com>
2015-07-20 17:49:04 -0400

    update

---
+++ AddressTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class AddressTest {
+
+	@Test
+	public void test() {
+		Address test = new Address("130","Farmstead Lane", 163, "PA", "State College", 16803);
+		Address test2 = new Address("130 1/2", "Farmstead Lane", "PA", "State College", 00053);
+		test.print();
+		test2.print();
+		test.comesBefore(test2);
+	}
+
+}
+
+/*
+ Junit output:
+ 
+ Street: 130 Farmstead Lane
+ Apartment Number: 163
+ City: State College, State: PA, Postal Code: 16803
+ Street: 130 1/2 Farmstead Lane
+ Apartment Number: 0
+ City: State College, State: PA, Postal Code: 43
+*/
