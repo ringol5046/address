53ddcfaa5a565b66cc008f2b1b887af86ad423df
ringol5046 <ringol5046@gmail.com>
2015-07-21 12:05:30 -0400

    update

--- AddressTest.java
+++ AddressTest.java
@@ -3,16 +3,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AddressTest {
-
-	@Test
-	public void test() {
-		Address test = new Address("130","Farmstead Lane", 163, "PA", "State College", "16803");
-		Address test2 = new Address("130 1/2", "Farmstead Lane", "PA", "State College", "00053");
-		test.print();
-		test2.print();
-		test.comesBefore(test2);
-		
-		assertEquals("Street: 130 Farmstead Lane \nApartment Number: 163 \nCity: State College, State: PA, Zip Code: 16803",test.print());
-	}
-
+    
+    @Test
+    public void test() {
+        Address test = new Address("130","Farmstead Lane", 163, "PA", "State College", "16803");
+        Address test2 = new Address("130 1/2", "Farmstead Lane", "PA", "State College", "00053");
+        test.print();
+        test2.print();
+        test.comesBefore(test2);
+        
+        assertEquals("Street: 130 Farmstead Lane\nApartment Number: 163\nCity: State College, State: PA, Zip Code: 16803",test.print());
+    }
+    
 }
