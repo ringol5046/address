3e932ebf29a410a447b7700f1acf239d2cb1c2ec
ringol5046 <ringol5046@gmail.com>
2015-07-21 11:54:18 -0400

    update

--- AddressTest.java
+++ AddressTest.java
@@ -3,26 +3,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AddressTest {
-    
-    @Test
-    public void test() {
-        Address test = new Address("130","Farmstead Lane", 163, "PA", "State College", "16803");
-        Address test2 = new Address("130 1/2", "Farmstead Lane", "PA", "State College", "00053");
-        test.print();
-        test2.print();
-        test.comesBefore(test2);
-    }
-    
+
+	@Test
+	public void test() {
+		Address test = new Address("130","Farmstead Lane", 163, "PA", "State College", "16803");
+		Address test2 = new Address("130 1/2", "Farmstead Lane", "PA", "State College", "00053");
+		test.print();
+		test2.print();
+		test.comesBefore(test2);
+		
+		assertEquals("Street: 130 Farmstead Lane \nApartment Number: 163 \nCity: State College, State: PA, Zip Code: 16803",test.print());
+	}
+
 }
-
-
-/*
- Junit output:
- 
- Street: 130 Farmstead Lane
- Apartment Number: 163
- City: State College, State: PA, Postal Code: 16803
- Street: 130 1/2 Farmstead Lane
- Apartment Number: 0
- City: State College, State: PA, Postal Code: 00053
-*/
